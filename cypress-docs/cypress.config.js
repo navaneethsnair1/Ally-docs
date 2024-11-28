12dcab1b97ff9087165c59345dc8c4f7bb20d5b7
navaneethsnair1 <Navaneeth.S.Nair1@ibm.com>
2024-11-28 14:22:29 +0530

    spec pattern added

--- cypress-docs/cypress.config.js
+++ cypress-docs/cypress.config.js
@@ -6,6 +6,7 @@ const yaml = require('js-yaml');
 module.exports = defineConfig({
   e2e: {
     supportFile: "/__w/Ally-docs/Ally-docs/cypress-docs/cypress/support/e2e.js",
+    specPattern: '/__w/Ally-docs/Ally-docs/cypress-docs/cypress/e2e/*.cy.js',
     setupNodeEvents(on, config) {
       on('task', {
         accessibilityChecker: require('cypress-accessibility-checker/plugin')
