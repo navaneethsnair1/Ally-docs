e1eb70fbbe683729e7f6e429702ec5326cfcc432
navaneethsnair1 <Navaneeth.S.Nair1@ibm.com>
2024-12-18 16:37:11 +0530

    debugging

--- cypress-docs/cypress/support/e2e.js
+++ cypress-docs/cypress/support/e2e.js
@@ -18,3 +18,8 @@ import './commands'
 import 'cypress-accessibility-checker';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
